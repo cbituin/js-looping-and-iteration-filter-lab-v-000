daa6f8c3931a7321d355052fa4562c9ff2043db0
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-19 18:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Code your solution in this file
 const findMatching = (arr, name) => {
-  let matchingData = arr.push(arr.filter(val => val === name));
+  let matchingData = [];
+  matchingData.push(arr.filter(val => val === name));
+  return matchingData;
 };
+
 
 const fuzzyMatch = () => {
   
